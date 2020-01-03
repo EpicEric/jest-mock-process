a2b4ce913a44efc7f2037dcac2fd3a462eeb96f8
Paulo Köch <paulo.koch@gmail.com>
2020-01-03 02:34:07 +0000

    Introduce spyOnImplementing

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,13 @@
+const maybeMockRestore = (a: any): void => a.mockRestore ? a.mockRestore() : undefined
+
+export function spyOnImplementing<
+    T extends object,
+    M extends keyof T,
+    F extends T[M] extends (...args: any[]) => any ? T[M] : never,
+>(target: T, property: M, impl: F): jest.SpyInstance  {
+    maybeMockRestore(target[property])
+    return jest.spyOn(target, property).mockImplementation(impl)
+}
 /**
  * Helper function to create a mock of the Node.js method
  * `process.exit(code: number)`.
@@ -5,76 +15,38 @@
  * @param {Object} err Optional error to raise. If unspecified or falsy, calling `process.exit` will resume code
  * execution instead of raising an error.
  */
-export function mockProcessExit(err?: any) {
-    const processExit = process.exit as any;
-    if (processExit.mockRestore) {
-        processExit.mockRestore();
-    }
-    let spyImplementation: any;
-    if (err) {
-        spyImplementation = jest.spyOn(process, 'exit')
-            .mockImplementation((_?: number) => {throw err});
-    } else {
-        spyImplementation = (jest.spyOn(process, 'exit') as any)
-            .mockImplementation((_?: number) => {});
-    }
-    return spyImplementation as jest.SpyInstance<(
-        code?: number
-    ) => never>;
-};
+export const mockProcessExit = (err?: any) => spyOnImplementing(
+    process,
+    'exit',
+    err ? (_?: number) => {throw err} : ((_?: number) => {}) as (_?: number) => never
+)
 
 /**
  * Helper function to create a mock of the Node.js method
  * `process.stdout.write(text: string, callback?: function): boolean`.
  */
-export function mockProcessStdout() {
-    const processStdout = process.stdout.write as any;
-    if (processStdout.mockRestore) {
-        processStdout.mockRestore();
-    }
-    let spyImplementation: any;
-    spyImplementation = jest.spyOn(process.stdout, 'write')
-        .mockImplementation(() => true);
-    return spyImplementation as jest.SpyInstance<(
-        buffer: Buffer | string,
-        encoding?: string,
-        cb?: Function
-    ) => boolean>;
-};
+export const mockProcessStdout = () => spyOnImplementing(
+    process.stdout,
+    'write',
+    () => true,
+)
 
 /**
  * Helper function to create a mock of the Node.js method
  * `process.stderr.write(text: string, callback?: function): boolean`.
  */
-export function mockProcessStderr() {
-    const processStderr = process.stderr.write as any;
-    if (processStderr.mockRestore) {
-        processStderr.mockRestore();
-    }
-    let spyImplementation: any;
-    spyImplementation = jest.spyOn(process.stderr, 'write')
-        .mockImplementation(() => true);
-    return spyImplementation as jest.SpyInstance<(
-        buffer: Buffer | string,
-        encoding?: string,
-        cb?: Function
-    ) => boolean>;
-};
+export const mockProcessStderr = () => spyOnImplementing(
+    process.stderr,
+    'write',
+    () => true,
+)
 
 /**
  * Helper function to create a mock of the Node.js method
  * `console.log(message: any)`.
  */
-export function mockConsoleLog() {
-    const consoleLog = console.log as any;
-    if (consoleLog.mockRestore) {
-        consoleLog.mockRestore();
-    }
-    let spyImplementation: any;
-    spyImplementation = jest.spyOn(console, 'log')
-        .mockImplementation(() => {});
-    return spyImplementation as jest.SpyInstance<(
-        message?: any,
-        ...optionalParams: any[]
-    ) => void>;
-};
+export const mockConsoleLog = () => spyOnImplementing(
+    console,
+    'log',
+    () => {},
+)
